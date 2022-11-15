cbe07d9e8c6440f71d2813dda0127ca4965ae2ef
Lee, YoungHoon <nobel6018@gmail.com>
2022-11-16 07:54:08 +0900

    refac: remove key code wrapper condition statement

--- content-script.js
+++ content-script.js
@@ -14,117 +14,108 @@ if (window === top) {
 }
 
 function keyPressLogic(e) {
-  if (
-    (e.shiftKey && e.keyCode === 190) ||
-    (e.shiftKey && e.keyCode === 188) ||
-    e.keyCode === 38 ||
-    e.keyCode === 40 ||
-    e.keyCode === 39 ||
-    e.keyCode === 37
-  ) {
-    const video = document.getElementsByTagName('video')[0];
+  const video = document.getElementsByTagName('video')[0];
 
-    // keypress: >
-    if (e.shiftKey && e.keyCode === 190) {
-      preLogic();
+  // keypress: >
+  if (e.shiftKey && e.keyCode === 190) {
+    preLogic();
 
-      document.querySelector('div.ytp-bezel-text').textContent = `${video.playbackRate.toString()}x`;
+    document.querySelector('div.ytp-bezel-text').textContent = `${video.playbackRate.toString()}x`;
 
-      const volumeUpIcon = `<svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
+    const volumeUpIcon = `<svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
           <use class='ytp-svg-shadow' xlink:href='#ytp-id-216'></use>
           <path class='ytp-svg-fill' d='M 10,24 18.5,18 10,12 V 24 z M 19,12 V 24 L 27.5,18 19,12 z'
                 id='ytp-id-216'></path>
         </svg>`;
-      setBezelIcon(volumeUpIcon);
+    setBezelIcon(volumeUpIcon);
 
-      document.querySelector('div#ytp-bezel-wrapper').classList.remove('ytp-bezel-text-hide');
-      postLogic();
-    }
+    document.querySelector('div#ytp-bezel-wrapper').classList.remove('ytp-bezel-text-hide');
+    postLogic();
+  }
 
-    // keypress: <
-    if (e.shiftKey && e.keyCode === 188) {
-      preLogic();
+  // keypress: <
+  if (e.shiftKey && e.keyCode === 188) {
+    preLogic();
 
-      document.querySelector('div.ytp-bezel-text').textContent = `${video.playbackRate.toString()}x`;
+    document.querySelector('div.ytp-bezel-text').textContent = `${video.playbackRate.toString()}x`;
 
-      const volumeDownIcon = `<svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
+    const volumeDownIcon = `<svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
           <use class='ytp-svg-shadow' xlink:href='#ytp-id-242'></use>
           <path class='ytp-svg-fill' d='M 17,24 V 12 l -8.5,6 8.5,6 z m .5,-6 8.5,6 V 12 l -8.5,6 z'
                 id='ytp-id-242'></path>
         </svg>`;
-      setBezelIcon(volumeDownIcon);
+    setBezelIcon(volumeDownIcon);
 
-      document.querySelector('div#ytp-bezel-wrapper').classList.remove('ytp-bezel-text-hide');
-      postLogic();
-    }
+    document.querySelector('div#ytp-bezel-wrapper').classList.remove('ytp-bezel-text-hide');
+    postLogic();
+  }
 
-    // keypress: ↑
-    if (e.keyCode === 38) {
-      preLogic();
+  // keypress: ↑
+  if (e.keyCode === 38) {
+    preLogic();
 
-      const volume = Math.round(video.volume * 100);
-      document.querySelector('div.ytp-bezel-text').textContent = `${volume}%`;
+    const volume = Math.round(video.volume * 100);
+    document.querySelector('div.ytp-bezel-text').textContent = `${volume}%`;
 
-      const volumeUpIcon = `<svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
+    const volumeUpIcon = `<svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
           <use class='ytp-svg-shadow' xlink:href='#ytp-id-371'></use>
           <path d='M8,21 L12,21 L17,26 L17,10 L12,15 L8,15 L8,21 Z M19,14 L19,22 C20.48,21.32 21.5,19.77 21.5,18 C21.5,16.26 20.48,14.74 19,14 Z M19,11.29 C21.89,12.15 24,14.83 24,18 C24,21.17 21.89,23.85 19,24.71 L19,26.77 C23.01,25.86 26,22.28 26,18 C26,13.72 23.01,10.14 19,9.23 L19,11.29 Z'
                 fill='#fff' id='ytp-id-371'></path>
         </svg>`;
-      setBezelIcon(volumeUpIcon);
+    setBezelIcon(volumeUpIcon);
 
-      document.querySelector('div#ytp-bezel-wrapper').classList.remove('ytp-bezel-text-hide');
-      postLogic();
-    }
+    document.querySelector('div#ytp-bezel-wrapper').classList.remove('ytp-bezel-text-hide');
+    postLogic();
+  }
 
-    // keypress: ↓
-    if (e.keyCode === 40) {
-      preLogic();
+  // keypress: ↓
+  if (e.keyCode === 40) {
+    preLogic();
 
-      const volume = Math.round(video.volume * 100);
-      document.querySelector('div.ytp-bezel-text').textContent = `${volume}%`;
+    const volume = Math.round(video.volume * 100);
+    document.querySelector('div.ytp-bezel-text').textContent = `${volume}%`;
 
-      if (video.volume === 0) {
-        const muteIcon = `<svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
+    if (video.volume === 0) {
+      const muteIcon = `<svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
             <use class='ytp-svg-shadow' xlink:href='#ytp-id-310'></use>
             <path class='ytp-svg-fill'
                   d='m 21.48,17.98 c 0,-1.77 -1.02,-3.29 -2.5,-4.03 v 2.21 l 2.45,2.45 c .03,-0.2 .05,-0.41 .05,-0.63 z m 2.5,0 c 0,.94 -0.2,1.82 -0.54,2.64 l 1.51,1.51 c .66,-1.24 1.03,-2.65 1.03,-4.15 0,-4.28 -2.99,-7.86 -7,-8.76 v 2.05 c 2.89,.86 5,3.54 5,6.71 z M 9.25,8.98 l -1.27,1.26 4.72,4.73 H 7.98 v 6 H 11.98 l 5,5 v -6.73 l 4.25,4.25 c -0.67,.52 -1.42,.93 -2.25,1.18 v 2.06 c 1.38,-0.31 2.63,-0.95 3.69,-1.81 l 2.04,2.05 1.27,-1.27 -9,-9 -7.72,-7.72 z m 7.72,.99 -2.09,2.08 2.09,2.09 V 9.98 z'
                   id='ytp-id-310'></path>
             </svg>`;
-        setBezelIcon(muteIcon);
-      } else {
-        const volumeDownIcon = `<svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
+      setBezelIcon(muteIcon);
+    } else {
+      const volumeDownIcon = `<svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
             <use class='ytp-svg-shadow' xlink:href='#ytp-id-299'></use>
             <path d='M8,21 L12,21 L17,26 L17,10 L12,15 L8,15 L8,21 Z M19,14 L19,22 C20.48,21.32 21.5,19.77 21.5,18 C21.5,16.26 20.48,14.74 19,14 Z'
                   fill='#fff' id='ytp-id-299'></path>
           </svg>`;
-        setBezelIcon(volumeDownIcon);
-      }
-
-      document.querySelector('div#ytp-bezel-wrapper').classList.remove('ytp-bezel-text-hide');
-      postLogic();
+      setBezelIcon(volumeDownIcon);
     }
 
-    // kepress: →
-    if (e.keyCode === 39) {
-      const seeking = document.querySelector('div.ytp-doubletap-ui-legacy');
-      seeking.style.display = '';
-      seeking.setAttribute('data-side', 'forward');
+    document.querySelector('div#ytp-bezel-wrapper').classList.remove('ytp-bezel-text-hide');
+    postLogic();
+  }
 
-      restartSeekingAnimation();
+  // kepress: →
+  if (e.keyCode === 39) {
+    const seeking = document.querySelector('div.ytp-doubletap-ui-legacy');
+    seeking.style.display = '';
+    seeking.setAttribute('data-side', 'forward');
 
-      postLogic();
-    }
+    restartSeekingAnimation();
 
-    // keypress: ←
-    if (e.keyCode === 37) {
-      const seeking = document.querySelector('div.ytp-doubletap-ui-legacy');
-      seeking.style.display = '';
-      seeking.setAttribute('data-side', 'back');
+    postLogic();
+  }
 
-      restartSeekingAnimation();
+  // keypress: ←
+  if (e.keyCode === 37) {
+    const seeking = document.querySelector('div.ytp-doubletap-ui-legacy');
+    seeking.style.display = '';
+    seeking.setAttribute('data-side', 'back');
 
-      postLogic();
-    }
+    restartSeekingAnimation();
+
+    postLogic();
   }
 }
 
